695d96549f79aef0378bd3710417e817d3060925
JongPil-lee <jplee@langcode.io>
2024-05-31 18:57:29 +0900

    semantickernel Test

---
+++ Kor-IT-ConsoleProject/SemanticKernel/Step1_CreateKernel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+namespace Kor_IT_ConsoleProject.SemanticKernel
+{
+	public class Step1_CreateKernel
+	{
+		Kernel kernel = Kernel.CreateBuilder().AddOpenAIChatCompletion("")
+
+	}
+}
