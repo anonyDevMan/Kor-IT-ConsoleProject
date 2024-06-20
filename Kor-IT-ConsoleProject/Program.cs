ff8eef3f0c4ce494a427e76d08158ced20fec5ab
JongPil Lee <jplee@langcode.io>
2024-06-20 18:00:31 +0900

    Step1 Semantickernel Complete

--- Kor-IT-ConsoleProject/Program.cs
+++ Kor-IT-ConsoleProject/Program.cs
@@ -2,31 +2,32 @@
 using Kor_IT_ConsoleProject.SemanticKernel;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using Microsoft.SemanticKernel.Services;
-using System;
-using System.IO;
-using System.Runtime;
 
 internal class Program
 {
-	private static void Main(string[] args)
-	{
-		var builder = new ConfigurationBuilder()
-	.SetBasePath(Directory.GetCurrentDirectory())
-	.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+    private static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
-		IConfiguration config = builder.Build();
+        IConfiguration config = builder.Build();
 
-		IServiceCollection services = new ServiceCollection();
-		services.Configure<LLMOption>(config.GetSection("LLMOptions"));
+        IServiceCollection services = new ServiceCollection();
+        services.Configure<LLMOption>(config.GetSection("LLMOptions"));
 
-		services.AddTransient<Step1_CreateKernel>();
-		
-		var serviceProvider = services.BuildServiceProvider();
+        services.AddTransient<Step1_CreateKernel>();
 
-		var step1 = serviceProvider.GetService<Step1_CreateKernel>();
+        var serviceProvider = services.BuildServiceProvider();
 
-		step1.Call();
-	}
+        var step1 = serviceProvider.GetService<Step1_CreateKernel>();
+
+        try
+        {
+            Task.Run(async () => await step1.Call()).Wait();
+        }
+        catch (Exception ex)
+        {
+        }
+    }
 }
--- Kor-IT-ConsoleProject/SemanticKernel/Step1_CreateKernel.cs
+++ Kor-IT-ConsoleProject/SemanticKernel/Step1_CreateKernel.cs
@@ -1,34 +1,77 @@
 ﻿using Kor_IT_ConsoleProject.Options;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
-using System.Diagnostics;
 
 namespace Kor_IT_ConsoleProject.SemanticKernel
 {
-	public class Step1_CreateKernel
-	{
-		private IConfiguration _configuration;
-		private LLMOption _llmOption;
+    public class Step1_CreateKernel
+    {
+        private LLMOption _llmOption;
+        private AzureOpenAIOption _azureOpenAIOption;
+        private Kernel _kernel;
 
-		public Step1_CreateKernel(IOptions<LLMOption> option)
+        public Step1_CreateKernel(IOptions<LLMOption> llmOption)
         {
-			//_configuration = configuration;
-			_llmOption = option.Value;
+            _llmOption = llmOption.Value;
+
+            _azureOpenAIOption = _llmOption.AzureOpenAIOptions.FirstOrDefault();
+
+            _kernel = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(_azureOpenAIOption.DeployementName, _azureOpenAIOption.Endpoint, _azureOpenAIOption.Key).Build();
         }
 
-        //Kernel kernel = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion("");
+        public async Task Call()
+        {
+            Console.WriteLine("Start Call \n");
 
+            try
+            {
+                #region Step 1
 
-        public void Call()
-		{
-			//Console.WriteLine(_configuration["LLMOptions:AzureOpenAI"].ToString());
-			_llmOption.AzureOpenAIOptions.ForEach(option =>
-			{
-				Console.WriteLine(option.DeployementName);
-			});
-		}
+                //var promptResult = await _kernel.InvokePromptAsync("What color is the sky?");
 
-	}
+                //Console.WriteLine(promptResult);
+
+                #endregion Step 1
+
+                #region Step 2
+
+                //KernelArguments kernelArguments = new KernelArguments() { { "topic", "sea" } };
+                //var promptResult = await _kernel.InvokePromptAsync("What color is the {{$topic}}? Please, answer is Korean.", kernelArguments);
+
+                //Console.WriteLine(promptResult);
+
+                #endregion Step 2
+
+                #region Step 3
+
+                //KernelArguments kernelArguments = new KernelArguments() { { "topic", "sea" } };
+
+                //await foreach(var stream in _kernel.InvokePromptStreamingAsync("What color is the {{$topic}}? Provide a detailed explantion. At least 500 characters. Please, answer is Korean.", kernelArguments))
+                //{
+                //    if (stream.Metadata["FinishReason"] is not null && stream.Metadata["FinishReason"].Equals("stop"))
+                //        Console.WriteLine();
+                //    else
+                //        Console.Write(stream);
+                //}
+
+                #endregion Step 3
+
+                #region Step 4
+
+                //KernelArguments kernelArguments = new KernelArguments(new OpenAIPromptExecutionSettings { MaxTokens = 500, Temperature = 0.5 }) { { "topic", "dog" } };
+
+                //var promptResult = await _kernel.InvokePromptAsync("Tell me story about {{$topic}}? Please, answer is Korean.", kernelArguments);
+
+                //Console.WriteLine(promptResult);
+
+                #endregion Step 4
+            }
+            catch (Exception ex)
+            {
+            }
+
+            Console.WriteLine("\n End Call");
+        }
+    }
 }
